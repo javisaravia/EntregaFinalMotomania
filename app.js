e0e149a9daa5c97b3f56ed060becf7a1a986743f
javisaravia <fso0001@alu.medac.es>
2026-02-25 21:23:51 +0100

    Deployment: Actualización del dominio de producción a appmotomania.vercel.app

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.use(cors({
     origin: [
         'http://127.0.0.1:5500', 
         'http://localhost:5500', 
-        'https://motomania-frontend.vercel.app',
-        'https://entrega-final-motomania.vercel.app',
-        'https://entregafinalmotomania.vercel.app'
+        'https://appmotomania.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
